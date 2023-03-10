ed3fc3d6e767735c3af39a6059219d409e696fd3
Stacy Apeyon <stacyapeyon@gmail.com>
2023-03-10 13:36:50 +0300

    Once the buttons are clicked the numbers and operators are displayed on then input area. Also Implemented the decimal point functionality

--- app/src/main/java/com/example/mycalculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/mycalculatorapp/MainActivity.kt
@@ -3,15 +3,35 @@ package com.example.mycalculatorapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private var tvInput: TextView? = null
+    var lastNumber : Boolean = false
+    var lastDot : Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        fun onDigit(view: View){
-            Toast.makeText(this, "Button clicked!", Toast.LENGTH_LONG).show()
-        }
+        tvInput = findViewById(R.id.tvInput)
+    }
+    fun onDigit(view: View){
+//        Toast.makeText(this, "Button clicked!", Toast.LENGTH_LONG).show()
+        tvInput?.append((view as Button).text)
+        lastNumber = true
+        lastDot = false
+    }
+    fun clearEverything(view: View){
+        tvInput?.text = ""
+    }
+    fun onDecimalPoint(view: View){
+    if (lastNumber  && !lastDot) {
+        tvInput?.append(".")
+        lastNumber = false
+        lastDot = true
+    }
     }
 }
